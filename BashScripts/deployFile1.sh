2b03224b72707c76c313fe2bd005e0aba31e32e4
Gurki73 <81249774+Gurki73@users.noreply.github.com>
2023-02-13 14:33:17 +0100

    Update deployFile1.sh

--- BashScripts/deployFile1.sh
+++ BashScripts/deployFile1.sh
@@ -6,9 +6,6 @@ do
   echo "$arg"
 done
 
-git pull "https://$1:$2@github.com/Brights-DevOps-2022-Script/DevOps-Daemons.git HEAD:main"
-git checkout main 
-
   echo 'apiVersion: kustomize.config.k8s.io/v1beta1             >  ./argocd/kustomize.yaml
   echo" kind: Kustomization"                                    >> ./argocd/kustomize.yaml
   echo" resources:"                                             >> ./argocd/kustomize.yaml  
@@ -17,7 +14,5 @@ git checkout main
   echo "  - name: nginxcomic"                                   >> ./argocd/kustomize.yaml
   echo "newName: $3:$4" >> ./kustomize.yaml
 
-git add ./kustomize.yaml"
-git commit -m 'kustom [skip ci]'"
-git push "https://${GIT_USERNAME}:$5 +'HEAD:main'"
+
 
