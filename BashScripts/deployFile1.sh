fa502e7ffab8a140cdd0139c06d1988fd96307a2
Gurki73 <81249774+Gurki73@users.noreply.github.com>
2023-02-13 16:51:39 +0100

    changed yaml file name

--- BashScripts/deployFile1.sh
+++ BashScripts/deployFile1.sh
@@ -9,7 +9,7 @@ done
   echo" apiVersion: kustomize.config.k8s.io/v1beta1"            >  ./yml-Files/kustomize.yml
   echo" kind: Kustomization"                                    >> ./yml-Files/kustomize.yml
   echo" resources:"                                             >> ./yml-Files/kustomize.yml 
-  echo"  - nginx.yml"                                           >> ./yml-Files/kustomize.yml
+  echo"  - deployment.service.ingress.yml."                     >> ./yml-Files/kustomize.yml
   echo" images:"                                                >> ./yml-Files/kustomize.yml
   echo "  - name: nginxcomic"                                   >> ./yml-Files/kustomize.yml
   echo "newName: ${1}:${2}"                                     >> ./yml-Files/kustomize.yml
