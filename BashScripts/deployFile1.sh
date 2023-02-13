db3e857f83663423b791fe776d07fb74db0ac975
Gurki73 <81249774+Gurki73@users.noreply.github.com>
2023-02-13 16:19:29 +0100

    added quotes

--- BashScripts/deployFile1.sh
+++ BashScripts/deployFile1.sh
@@ -6,7 +6,7 @@ do
   echo "$arg"
 done
 
-  echo 'apiVersion: kustomize.config.k8s.io/v1beta1             >  ./yml-Files/kustomize.yml
+  echo" apiVersion: kustomize.config.k8s.io/v1beta1"            >  ./yml-Files/kustomize.yml
   echo" kind: Kustomization"                                    >> ./yml-Files/kustomize.yml
   echo" resources:"                                             >> ./yml-Files/kustomize.yml 
   echo"  - nginx.yml"                                           >> ./yml-Files/kustomize.yml
