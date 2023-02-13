fd3552b28bbd93b8ddfd0915fecb56771fb01bb1
Gurki73 <81249774+Gurki73@users.noreply.github.com>
2023-02-13 17:21:24 +0100

    Update deployFile2.sh

--- BashScripts/deployFile2.sh
+++ BashScripts/deployFile2.sh
@@ -2,14 +2,14 @@ echo inside
 git pull https://$1:$2@github.com/Brights-DevOps-2022-Script/DevOps-Daemons.git HEAD:main
 git checkout main
 
-echo "apiVersion: kustomize.config.k8s.io/v1beta1"                    >  ./argocd/kustomize.yaml
-echo "kind: Kustomization"                                            >> ./argocd/kustomize.yaml
-echo "  resources:"                                                   >> ./argocd/kustomize.yaml
-echo "   - nginx.yml"                                                 >> ./argocd/kustomize.yaml
-echo "     images:"                                                   >> ./argocd/kustomize.yaml
-echo "      - name: felixstr2"                                        >> ./argocd/kustomize.yaml
-echo "        newName: devops2022.azurecr.io/nginxanis:${GIT_COMMIT}" >> ./argocd/kustomize.yaml
+echo "apiVersion: kustomize.config.k8s.io/v1beta1"                    >  ./yml-Files/kustomization.yml
+echo "kind: Kustomization"                                            >> ./yml-Files/kustomization.yml
+echo "  resources:"                                                   >> ./yml-Files/kustomization.yml
+echo "   - nginx.yml"                                                 >> ./yml-Files/kustomization.yml
+echo "     images:"                                                   >> ./yml-Files/kustomization.yml
+echo "      - name: felixstr2"                                        >> ./yml-Files/kustomization.yml
+echo "        newName: devops2022.azurecr.io/nginxanis:${GIT_COMMIT}" >> ./yml-Files/kustomization.yml
 
-git add .kustomize.yaml
+git add .kustomization.yml.yaml
 git commit -m 'made with extenal bash script deployFile2.sh'
 git push https://$1:$2@github.com/Brights-DevOps-2022-Script/team-3-argoTest.git HEAD:main
