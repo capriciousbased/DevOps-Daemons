024abc7e135ef2b89197b0fd8f535810487f1587
Gurki73 <81249774+Gurki73@users.noreply.github.com>
2023-02-13 14:46:12 +0100

    Update deployFile2.sh

--- BashScripts/deployFile2.sh
+++ BashScripts/deployFile2.sh
@@ -1,5 +1,5 @@
 echo inside 
-git pull https://$1:$2@github.com/Brights-DevOps-2022-Script/team-3-argoTest.git HEAD:main
+git pull https://$1:$2@github.com/Brights-DevOps-2022-Script/DevOps-Daemons.git HEAD:main
 git checkout main
 
 echo "apiVersion: kustomize.config.k8s.io/v1beta1"                    >  ./argocd/kustomize.yaml
@@ -10,6 +10,6 @@ echo "     images:"                                                   >> ./argoc
 echo "      - name: felixstr2"                                        >> ./argocd/kustomize.yaml
 echo "        newName: devops2022.azurecr.io/nginxanis:${GIT_COMMIT}" >> ./argocd/kustomize.yaml
 
-git add ../argocd/kustomize.yaml
+git add .kustomize.yaml
 git commit -m 'made with extenal bash script deployFile2.sh'
 git push https://$1:$2@github.com/Brights-DevOps-2022-Script/team-3-argoTest.git HEAD:main
