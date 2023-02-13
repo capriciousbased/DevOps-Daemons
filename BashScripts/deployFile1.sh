3cb8664124dee8ec89f3a4ee060c9243776689d3
FluffynatorT800 <keineidee1@gmx.de>
2023-02-13 18:45:25 +0100

    changed name to allinone in deployfile1

--- BashScripts/deployFile1.sh
+++ BashScripts/deployFile1.sh
@@ -9,9 +9,7 @@ done
   echo "apiVersion: kustomize.config.k8s.io/v1beta1"            >  ./yml-Files/kustomization.yml
   echo "kind: Kustomization"                                    >> ./yml-Files/kustomization.yml
   echo "resources:"                                             >> ./yml-Files/kustomization.yml
-  echo "  - ingress.yml"                                        >> ./yml-Files/kustomization.yml
-  echo "  - service.yml"                                        >> ./yml-Files/kustomization.yml
-  echo "  - deployment.yml"                                     >> ./yml-Files/kustomization.yml
+  echo "  - allinone.yml"                                        >> ./yml-Files/kustomization.yml
   echo "images:"                                                >> ./yml-Files/kustomization.yml
   echo "  - name: nginxcomic"                                   >> ./yml-Files/kustomization.yml
   echo "    newName: ${1}:${2}"                                 >> ./yml-Files/kustomization.yml
