61d55b64963dbfcea187254a231f3c49f3273dfb
Gurki73 <81249774+Gurki73@users.noreply.github.com>
2023-02-13 20:36:30 +0100

    exterminated test

--- BashScripts/deployFile1.sh
+++ BashScripts/deployFile1.sh
@@ -9,8 +9,7 @@ done
   echo "apiVersion: kustomize.config.k8s.io/v1beta1"            >  ./yml-Files/kustomization.yml
   echo "kind: Kustomization"                                    >> ./yml-Files/kustomization.yml
   echo "resources:"                                             >> ./yml-Files/kustomization.yml
-  echo "  - allinone.yml"                                        >> ./yml-Files/kustomization.yml
+  echo "  - allinone.yml"                                       >> ./yml-Files/kustomization.yml
   echo "images:"                                                >> ./yml-Files/kustomization.yml
   echo "  - name: nginxcomic"                                   >> ./yml-Files/kustomization.yml
- # echo "    newName: devops2022.azurecr.io/${1}:${2}"           >> ./yml-Files/kustomization.yml
-  echo "    newName: devops2022.azurecr.io/comicbook:2a05b48cc70054ac3e327d28193abe012aa5d7f3" >> ./yml-Files/kustomization.yml
+  echo "    newName: devops2022.azurecr.io/${1}:${2}"           >> ./yml-Files/kustomization.yml
