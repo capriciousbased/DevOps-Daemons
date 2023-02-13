8ba7523f75dedaa3226fc28f099670527d77a89b
Gurki73 <81249774+Gurki73@users.noreply.github.com>
2023-02-13 20:33:34 +0100

    test

--- BashScripts/deployFile1.sh
+++ BashScripts/deployFile1.sh
@@ -12,4 +12,5 @@ done
   echo "  - allinone.yml"                                        >> ./yml-Files/kustomization.yml
   echo "images:"                                                >> ./yml-Files/kustomization.yml
   echo "  - name: nginxcomic"                                   >> ./yml-Files/kustomization.yml
-  echo "    newName: devops2022.azurecr.io/${1}:${2}"           >> ./yml-Files/kustomization.yml
+ # echo "    newName: devops2022.azurecr.io/${1}:${2}"           >> ./yml-Files/kustomization.yml
+  echo "    newName: devops2022.azurecr.io/comicbook:2a05b48cc70054ac3e327d28193abe012aa5d7f3" >> ./yml-Files/kustomization.yml
