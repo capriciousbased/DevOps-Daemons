f7af87e587166c45a2e9f21a82b3a8ddb87757fc
FluffynatorT800 <keineidee1@gmx.de>
2023-02-13 18:36:15 +0100

    renamed file, removed unecessary ending

--- BashScripts/deployFile2.sh
+++ BashScripts/deployFile2.sh
@@ -10,6 +10,6 @@ echo "     images:"                                                   >> ./yml-F
 echo "      - name: felixstr2"                                        >> ./yml-Files/kustomization.yml
 echo "        newName: devops2022.azurecr.io/nginxanis:${GIT_COMMIT}" >> ./yml-Files/kustomization.yml
 
-git add .kustomization.yml.yaml
+git add .kustomization.yml
 git commit -m 'made with extenal bash script deployFile2.sh'
 git push https://$1:$2@github.com/Brights-DevOps-2022-Script/team-3-argoTest.git HEAD:main
