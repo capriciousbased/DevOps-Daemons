74fb12f38e51c39cd86cbb86e304c74fa868a904
FluffynatorT800 <keineidee1@gmx.de>
2023-02-14 12:24:40 +0100

    changed app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,26 @@
-import React from "react"
-import { Footer } from "./components/footer/Footer"
-import { Header } from "./components/header/Header"
-import { Home } from "./pages/home/Home"
-import { Login } from "./pages/login/Login"
-import { Regsiter } from "./pages/login/Regsiter"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import { DetailsPages } from "./pages/details/DetailsPages"
-import { Account } from "./pages/account/Account"
-import { Create } from "./components/create/Create"
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <>
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Regsiter} />
-          <Route exact path='/details/:id' component={DetailsPages} />
-          <Route exact path='/account' component={Account} />
-          <Route exact path='/create' component={Create} />
-        </Switch>
-        <Footer />
-      </Router>
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-export default App
+
+export default App;
