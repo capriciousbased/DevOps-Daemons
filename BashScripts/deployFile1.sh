f998c8d865809bd39fa4b578e293c3d3c101980b
Gurki73 <j.miethe73@gmail.com>
2023-02-15 14:35:14 +0100

    force

--- BashScripts/deployFile1.sh
+++ BashScripts/deployFile1.sh
@@ -1,9 +1,9 @@
 
-echo "apiVersion: kustomize.config.k8s.io/v1beta1"    >  ./yml-Files/kustomize.yml
-echo "kind: Kustomization"                            >> ./yml-Files/kustomize.yml
-echo "resources:"                                     >> ./yml-Files/kustomize.yml
-echo "  - allinone.yml"                               >> ./yml-Files/kustomize.yml
-echo "images:"                                        >> ./yml-Files/kustomize.yml
-echo "   - name: nginxcomic"                          >> ./yml-Files/kustomize.yml
-echo "     newName: devops2022.azurecr.io/${1}"       >> ./yml-Files/kustomize.yml
-echo "     newTag: ${2}"                              >> ./yml-Files/kustomize.yml
+echo "apiVersion: kustomize.config.k8s.io/v1beta1"    >  ./yml-Files/kustomization.yml
+echo "kind: Kustomization"                            >> ./yml-Files/kustomization.yml
+echo "resources:"                                     >> ./yml-Files/kustomization.yml
+echo "  - allinone.yml"                               >> ./yml-Files/kustomization.yml
+echo "images:"                                        >> ./yml-Files/kustomization.yml
+echo "   - name: nginxcomic"                          >> ./yml-Files/kustomization.yml
+echo "     newName: devops2022.azurecr.io/${1}"       >> ./yml-Files/kustomization.yml
+echo "     newTag: ${2}"                              >> ./yml-Files/kustomization.yml
