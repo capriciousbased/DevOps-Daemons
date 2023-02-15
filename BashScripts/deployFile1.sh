3442d75621e9c6731a38d39ae1a42961e876a44c
Gurki73 <j.miethe73@gmail.com>
2023-02-15 11:58:13 +0100

    force

--- BashScripts/deployFile1.sh
+++ BashScripts/deployFile1.sh
@@ -1,3 +1,8 @@
+if [ "$#" -lt 2 ]; then
+  echo "Usage: $0 <image1> <tag1> [<image2> <tag2> ...]"
+  exit 1
+fi
+  
   echo "apiVersion: kustomize.config.k8s.io/v1beta1"            >  ./yml-Files/kustomization.yml
   echo "kind: Kustomization"                                    >> ./yml-Files/kustomization.yml
   echo "resources:"                                             >> ./yml-Files/kustomization.yml
