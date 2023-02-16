30876127fd1b16fc0dc8666369d89a64c6dda64d
FluffynatorT800 <keineidee1@gmx.de>
2023-02-16 18:28:07 +0100

    removed unecessary

--- BashScripts/deployFile1.sh
+++ BashScripts/deployFile1.sh
@@ -3,8 +3,6 @@ echo "apiVersion: kustomize.config.k8s.io/v1beta1"    >  ./yml-Files/kustomizati
 echo "kind: Kustomization"                            >> ./yml-Files/kustomization.yml
 echo "resources:"                                     >> ./yml-Files/kustomization.yml
 echo "  - allinone.yml"                               >> ./yml-Files/kustomization.yml
-echo "  - configmap.yml"                              >> ./yml-Files/kustomization.yml
-echo "  - pod.yml"                                    >> ./yml-Files/kustomization.yml
 echo "images:"                                        >> ./yml-Files/kustomization.yml
 echo "   - name: nginxcomic"                          >> ./yml-Files/kustomization.yml
 echo "     newName: devops2022.azurecr.io/${1}"       >> ./yml-Files/kustomization.yml
