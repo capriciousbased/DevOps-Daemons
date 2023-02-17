1ac498a41d09368e69794c4e866e5b63d3d2261f
Gurki73 <j.miethe73@gmail.com>
2023-02-17 22:41:02 +0100

    added import

--- frontendlatest/src/components/category/Category.jsx
+++ frontendlatest/src/components/category/Category.jsx
@@ -6,6 +6,7 @@ import "slick-carousel/slick/slick-theme.css"
 import Slider from "react-slick"
 import { GrFormPrevious } from "react-icons/gr"
 import { MdNavigateNext } from "react-icons/md"
+import Carousel from './'
 
 const SampleNextArrow = (props) => {
   const { onClick } = props
