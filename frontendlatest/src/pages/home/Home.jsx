596bdc1c162a14396b05af0eaa15430815242231
capriciousbased <mdanisulhoquekhan@gmail.com>
2023-02-23 15:08:13 +0100

    devops loop added.

--- frontendlatest/src/pages/home/Home.jsx
+++ frontendlatest/src/pages/home/Home.jsx
@@ -8,8 +8,11 @@ import Contact from "../contact/Contact";
 export const Home = () => {
   return (
     <>
-     {/*   <Slider /> */}
-      <Category />
+    {/*   <Category /> */}
+    <div className="devopsLoop">
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP9hzI33BXACnZclrYmJKceGcLCO7EN8Bf8g&usqp=CAU" alt="Trees"  />  
+    </div>
+   
       <Foreword />
       <Card />
       <Contact />
